a351cfe072597421dcf34e37e575854941fcf639
Jonny Snow <jonny.snow@cookieshq.co.uk>
2020-07-31 16:09:46 +0100

    font and dependancies

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,13 +37,15 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Indie+Flower&family=Raleway:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap" rel="stylesheet"' }
     ]
   },
   /*
   ** Global CSS
   */
   css: [
+    '@/assets/css/main.scss'
   ],
   /*
   ** Plugins to load before mounting the App
